549b768069f5059a71fa06455a070e3af3d6d3f2
Mobilpadde <mobilpadde@gmail.com>
2018-10-25 21:00:02 +0200

    Fix lazy-loading on error

--- site/scripts/src/lazy.js
+++ site/scripts/src/lazy.js
@@ -1,9 +1,17 @@
 import cheerio from 'cheerio';
 
+let n = 20181022;
+let status = 200;
+
 function load(n) {
     return new Promise((resolve, reject) =>
         fetch(`/log/${n}`)
-            .then((res) => res.text())
+            .then((res) => {
+                status = res.status;
+                if (status !== 200) throw new Error(status);
+                
+                return res.text();
+            })
             .then(cheerio.load)
             .then(($) => {
                 document.getElementById("list").innerHTML += $('#tinylog ul').html();
@@ -14,12 +22,14 @@ function load(n) {
 }
 
 window.addEventListener('load', () => {
-    load('20181022')
+    load(n++)
         .then(() => document
             .getElementById("list")
             .addEventListener('scroll', ({ target }) => {
-                if (target.scrollTopMax - 10 <= target.scrollTop) {
-                    load('20181023');
+                if (status === 200) {
+                    if (target.scrollTopMax - 10 <= target.scrollTop) {
+                        load(n++);
+                    }
                 }
             })
         )
