a855f3d4b8da9978c1527165e5cae0e4e3b8d179
Mobilpadde <mobilpadde@gmail.com>
2018-10-26 19:13:34 +0200

    Sort/merge types

--- src/tinyparser/types.nim
+++ src/tinyparser/types.nim
@@ -10,27 +10,41 @@ let thingMatcher = re"@(\w+)"
 let wholeThingMatcher = re"@(\w+)"
 
 proc parse*(lines: seq[string]): string =
-    var logs = newSeq[Log]()
+    var logs = @[
+        Log(thing: "bug"),
+        Log(thing: "fix"),
+        Log(thing: "new"),
+    ]
+
+    var current = 0
     for line in lines:
         let ln = line.replace("\n", "")
         if ln.find(thingMatcher).isSome():
-            var l = Log(
-                thing: ln
-                    .replace(re".*(?=@)", "")
-                    .replace(thingMatcher, "$1")
-                )
+            let thing = ln
+                .replace(re".*(?=@)", "")
+                .replace(thingMatcher, "$1")
+                .toLowerAscii()
 
-            logs.add(l)
+            case thing:
+                of "bug":
+                    current = 0
+                of "fix":
+                    current = 1
+                of "new":
+                    current = 2
         else:
             var res = ln.replace(re"([\\\n]\s?)+", "")
             for processor in zip(emphasis.matchers, emphasis.replacers):
                 let (matcher, replacer) = processor
                 res = nre.replace(res, matcher, replacer)
-            logs[logs.len() - 1].text &= res & "<br/>"
+            logs[current].text &= res & "<br/>"
 
     result = ""
     for l in logs:
-        result &= "<li class=\"$1\"><span class=\"$1\">$1</span><p>$2</p></li>" % [
-            l.thing,
-            l.text,
-        ]
+        if l.text.len() > 0:
+            result &= "<li class=\"$1\"><span class=\"$1\">$1</span><p>$2</p></li>" % [
+                l.thing,
+                l.text,
+            ]
+
+    return result
